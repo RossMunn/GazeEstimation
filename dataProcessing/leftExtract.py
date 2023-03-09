6b806f9e2514b58857e09e2f7e0d5a37f25cf84e
Ross Munn <ross.munn888@gmail.com>
2023-03-09 22:42:57 +0000

    Data preprocessing and model optimisation

--- dataProcessing/leftExtract.py
+++ dataProcessing/leftExtract.py
@@ -1,51 +1,86 @@
-import os
 import cv2
+import os
 import shutil
 
-# define the paths to the subfolders containing images
-folders_path = 'C:\\Users\\jrmun\\Desktop\\processeddataset'
+# Define the path to the directory containing the participant folders
+root_dir = 'C:\\Users\\jrmun\\Desktop\\MPIIGaze\\Data\\Original'
 
-# define the path to the haar cascade file for detecting eyes
-cascade_path = '/haarcascadeXML/haarcascade_eye.xml'
+# Define the path to the output directory for extracted images
+output_dir = 'C:\\Users\\jrmun\\Desktop\\MPIIGaze\\Data\\Extracted'
 
-# define the path to the output folder for the extracted left eye images
-output_folder_path = 'C:\\Users\\jrmun\\Desktop\\leftEye_Dataset'
+# Define the path to the haarcascade file
+cascade_file = 'C:\\Users\\jrmun\\PycharmProjects\\Disso\\haarcascadeXML\\haarcascade_mcs_lefteye.xml'
 
+# Load the haarcascade classifier
+cascade_classifier = cv2.CascadeClassifier(cascade_file)
 
-# create the output folder if it does not exist
-if not os.path.exists(output_folder_path):
-    os.makedirs(output_folder_path)
+# Loop through each participant folder
+for participant_name in os.listdir(root_dir):
+    # Define the path to the participant folder
+    participant_dir = os.path.join(root_dir, participant_name)
+    if not os.path.isdir(participant_dir):
+        continue
 
-# create a face detector object using the haar cascade file
-eye_cascade = cv2.CascadeClassifier(cascade_path)
+    print(f'Processing participant {participant_name}...')
 
-# loop over all subfolders
-for folder in os.listdir(folders_path):
-    subfolder_path = os.path.join(folders_path, folder)
+    # Loop through each day folder in the participant folder
+    for day_name in os.listdir(participant_dir):
+        # Skip the calibration folder
+        if day_name == 'Calibration':
+            continue
 
-    # create the corresponding subfolder in the output folder
-    output_subfolder_path = os.path.join(output_folder_path, folder)
-    if not os.path.exists(output_subfolder_path):
-        os.makedirs(output_subfolder_path)
+        print(f'Processing day {day_name}...')
 
-    # loop over all files in the subfolder
-    for file_name in os.listdir(subfolder_path):
-        image_path = os.path.join(subfolder_path, file_name)
+        # Define the path to the directory containing the images for this day
+        img_dir = os.path.join(participant_dir, day_name)
 
-        # read the image
-        image = cv2.imread(image_path)
+        # Define the path to the annotations file for this day
+        annot_file = os.path.join(participant_dir, day_name, 'annotation.txt')
 
-        # detect the eyes in the image
-        eyes = eye_cascade.detectMultiScale(image, scaleFactor=1.3, minNeighbors=5)
+        # Create a new directory for the extracted images for this day
+        output_day_dir = os.path.join(output_dir, participant_name, day_name)
+        os.makedirs(output_day_dir, exist_ok=True)
 
-        # loop over all detected eyes
-        for (x, y, w, h) in eyes:
-            # check if the detected eye is on the left side of the face
-            if x < image.shape[1] / 2:
-                # crop the left eye
-                left_eye = image[y:y + h, x:x + w]
+        # Copy the annotations file to the new directory for this day
+        shutil.copy(annot_file, output_day_dir)
 
-                # save the left eye to the corresponding subfolder in the output folder
-                output_file_name = 'left_eye_' + file_name
-                output_file_path = os.path.join(output_subfolder_path, output_file_name)
-                cv2.imwrite(output_file_path, left_eye)
+        # Load the annotations from the file for this day
+        with open(os.path.join(output_day_dir, 'annotation.txt'), 'r') as f:
+            annotations = [line.strip().split() for line in f]
+
+        # Create a counter for the image files
+        img_counter = 1
+
+        # Iterate through the annotations and extract the left eye images for this day
+        for annot in annotations:
+            # Load the corresponding image based on the file name
+            img_path = os.path.join(img_dir, f'{img_counter:04d}.jpg')
+            if not os.path.exists(img_path):
+                print(f'Could not find image file: {img_path}')
+                continue
+
+            # Load the image and convert to grayscale
+            img = cv2.imread(img_path)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Detect the eyes in the image using the haarcascade classifier
+            eyes = cascade_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+            # Extract the left eye image if it exists
+            left_eye = None
+            for (x, y, w, h) in eyes:
+                if x < img.shape[1] // 2:  # Check if the eye is on the left side of the image
+                    left_eye = img[y:y+h, x:x+w]
+                    break
+
+            if left_eye is not None:
+                # Save the left eye image to the output directory for this day
+                output_img_path = os.path.join(output_day_dir, f'{img_counter:04d}.jpg')
+                cv2.imwrite(output_img_path, left_eye)
+
+                # Increment the image file counter
+            img_counter += 1
+
+            print(f'Done processing day {day_name} + {img_counter}.\n')
+
+        print(f'Done processing participant {participant_name}.\n')
--- gazeEstimator/main.py
+++ gazeEstimator/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 # Define the path to the directory containing the participant folders
-root_dir = 'C:\\Users\\jrmun\\Desktop\\MPIIGaze\\Data\\Original'
+root_dir = 'C:\\Users\\jrmun\\Desktop\\MPIIGaze\\Data\\Extracted'
 
 # Loop through each participant folder
 for participant_name in os.listdir(root_dir):
@@ -40,22 +40,30 @@ for participant_name in os.listdir(root_dir):
         # Create a counter for the image files
         img_counter = 1
 
+        # Define the minimum width threshold
+        min_width = 62
+
         # Iterate through the annotations and load the corresponding image for this day
         for annot in annotations:
             # Extract the x and y values
             x = float(annot[24])
             y = float(annot[25])
-            # Append the x and y values to the data lists
-            x_data.append(x)
-            y_data.append(y)
             # Load the corresponding image based on the file name
             img_path = os.path.join(img_dir,
                                     f'{img_counter:04d}.jpg')  # Assumes images are named "0001.jpg", "0002.jpg", etc.
             if os.path.exists(img_path):
-                img = tf.keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
-                img_array = tf.keras.preprocessing.image.img_to_array(img)
-                img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
-                img_data.append(img_array)
+                img = tf.keras.preprocessing.image.load_img(img_path)
+                # Check if the image width is larger than the threshold
+                if img.size[0] >= min_width:
+                    img = img.resize((224, 224))
+                    img_array = tf.keras.preprocessing.image.img_to_array(img)
+                    img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
+                    img_data.append(img_array)
+                    # Append the x and y values to the data lists
+                    x_data.append(x)
+                    y_data.append(y)
+                else:
+                    print(f'Skipping image {img_path} because its width is too small')
             else:
                 print(f'Could not find image file: {img_path}')
             # Increment the image file counter
@@ -77,8 +85,8 @@ for participant_name in os.listdir(root_dir):
             tf.keras.layers.MaxPooling2D((2, 2)),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(64, activation='relu'),
-            tf.keras.layers.Dense(2)  # Two nodes for x and y values
-        ])
+            tf.keras.layers.Dense(2) # Two nodes for x and y values
+])
 
         # Compile the model
         model.compile(optimizer='adam',
@@ -94,4 +102,5 @@ for participant_name in os.listdir(root_dir):
         print(f'Done processing day {day_name}.\n')
 
     print(f'Done processing participant {participant_name}.\n')
+
 
